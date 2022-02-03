4adfaa2611c3f14fcc57406f0e41041b661c4665
Jose A. Vallejo <54599840+JoseAVallejo12@users.noreply.github.com>
2022-02-03 10:13:19 -0500

    create function   updateCount

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,9 +1,13 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
+let count = 0;
+
+function updateCounter() {
+  return count++;
+}
 
 $.when($.ready).then(function () {
-  let count = 0;
 
   $('body').append('<p>Holberton Dashboard</p>');
   $('body').append('<p>Dashboard data for the students</p>');
@@ -12,6 +16,6 @@ $.when($.ready).then(function () {
   $('body').append('<p>Copyright - Holberton School</p>');
 
   $('button').on('click', _.debounce(() => {
-    $('#count').text(`${count++} clicks on the button`);
+    $('#count').text(`${updateCounter()} clicks on the button`);
   }));
 });
