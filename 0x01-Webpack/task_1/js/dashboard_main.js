9dd1a536d1c50a8f926dd13d83002a840e6064e3
Jose A. Vallejo <54599840+JoseAVallejo12@users.noreply.github.com>
2022-02-03 09:34:51 -0500

    delete package-lock and fix task1

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -5,15 +5,12 @@ import * as _ from 'lodash';
 $.when($.ready).then(function () {
   let count = 0;
 
-  $('body').append(`
-    <p>Holberton Dashboard</p>
-    <p>Dashboard data for the students</p>
-    <buttom type="button">Click here to get started</buttom>
-    <p id='count'></p>
-    <p>Copyright - Holberton School</p>
-  `);
-
-  $('buttom').on('click', _.debounce(updateCounter, 300));
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<buttom type="button">Click here to get started</buttom>')
+    .on('click', _.debounce(updateCounter, 300));
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
 
   function updateCounter() {
     $('#count').text(`${count++} clicks on the button`);
