1d102e542214e7a8e25cd5d5abbb9d29d88dbc2f
Jose A. Vallejo <54599840+JoseAVallejo12@users.noreply.github.com>
2022-02-03 09:23:11 -0500

    fix index.js

--- 0x01-Webpack/task_0/src/index.js
+++ 0x01-Webpack/task_0/src/index.js
@@ -1,9 +1,7 @@
-import $ from 'jquery';
+import $ from "jquery";
 
-$.when($.ready).then(() => {
-  $('body').append(`
-    <p>Holberton Dashboard</p>
-    <p>Dashboard data for the students</p>
-    <p>Copyright - Holberton School</p>
-  `);
+$(function () {
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
 });
