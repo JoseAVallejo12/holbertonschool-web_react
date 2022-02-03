651a200d6c95a7fdc20869a6ca5d8db992cca4ca
Jose A. Vallejo <54599840+JoseAVallejo12@users.noreply.github.com>
2022-02-03 17:53:03 -0500

    fix ..

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -4,20 +4,18 @@ import _ from 'lodash';
 let count = 0;
 
 function updateCounter() {
-  count += 1;
-  return count;
+  return count++;
 }
 
 $(function () {
+
   $('body').append('<p>Holberton Dashboard</p>');
   $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button>Click here to get started</button>');
-  $('body').append("<p id='count'></p>");
+  $('body').append('<button type="button">Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
   $('body').append('<p>Copyright - Holberton School</p>');
 
-  let debouncedFunc = _.debounce(() => {
-    let count = updateCounter();
-    $('#count').text(`${count} clicks on the button`);
-  });
-  $('button').on('click', debouncedFunc);
+  $('button').on('click', _.debounce(() => {
+    $('#count').text(`${updateCounter()} clicks on the button`);
+  }));
 });
