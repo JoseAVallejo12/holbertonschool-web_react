9e3f87ea5613c74a60c7bc6aa7d2b1ab1af14aeb
Jose A. Vallejo <54599840+JoseAVallejo12@users.noreply.github.com>
2022-02-13 20:36:45 -0500

    jest integration work done

---
+++ 0x03-react_props/task_0/dashboard/src/App/App.js
@@ -0,0 +1,31 @@
+// @ts-nocheck
+import React from 'react'
+import { getFooterCopy, getFullYear } from '../utils/utils'
+import './App.css'
+import logo from '../assets/HolbertonLogo.jpg';
+
+export default function App() {
+
+  return (
+    <>
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" />
+        <button>OK</button>
+      </div>
+      <div className="App-footer">
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(true)}
+        </p>
+      </div>
+    </>
+
+  )
+}
